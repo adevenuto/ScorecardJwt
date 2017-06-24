9ae7ad323fed340d11be78428d5165f559960dfc
‘adevenuto’ <anthonydevenuto@gmail.com>
2017-06-24 14:04:08 +0000

    syncing

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,12 +1,12 @@
 @extends('main')
 @section('content')
 	<div class="jumbotron">
-		<h1>Index!</h1>
+		<h1>Index!!</h1>
 	</div>
 	<div class="jumbotron">
-		<h1>Index!</h1>
+		<h1>Index!!</h1>
 	</div>
 	<div class="jumbotron">
-		<h1>Index!</h1>
+		<h1>Index!!</h1>
 	</div>
 @endsection
