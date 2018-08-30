5430bfcbbfcfe6d7b34fe80da39c19003a4e88a9
‘adevenuto’ <anthonydevenuto@gmail.com>
2018-08-30 02:33:16 +0000

    sidebar close when overlay clicked

--- public/js/app.js
+++ public/js/app.js
@@ -12044,17 +12044,25 @@ $(function () {});
 
 // Slide In Nav
 $("#hamburger").on('click', function () {
-  $("#mySidenav").toggleClass('navIn');
-  $("#hamburger").toggleClass('open');
-  $("#overlay").toggleClass('overlay');
+	$("#mySidenav").toggleClass('navIn');
+	$("#hamburger").toggleClass('open');
+	$("#overlay").toggleClass('overlay');
 });
-
+// Slide In Close
+$("body").on('click', function (e) {
+	var target = e.target.id;
+	if (target === "overlay") {
+		$("#mySidenav").toggleClass('navIn');
+		$("#hamburger").toggleClass('open');
+		$("#overlay").toggleClass('overlay');
+	}
+});
 // Only Allow Numbers To Be Entered
 // Add class 'num-only' to input to prevent anything but numbers
 $('body').on('keypress', '.num-only', function (e) {
-  if (e.which < 48 || e.which > 57) {
-    e.preventDefault();
-  }
+	if (e.which < 48 || e.which > 57) {
+		e.preventDefault();
+	}
 });
 
 /***/ }),
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -7,7 +7,15 @@ $("#hamburger").on('click', function() {
 	$("#hamburger").toggleClass('open');
 	$("#overlay").toggleClass('overlay');
 })
-
+// Slide In Close
+$("body").on('click', function(e) {
+	let target = e.target.id;
+	if (target === "overlay") {
+		$("#mySidenav").toggleClass('navIn');
+		$("#hamburger").toggleClass('open');
+		$("#overlay").toggleClass('overlay');
+	}
+})
 // Only Allow Numbers To Be Entered
 // Add class 'num-only' to input to prevent anything but numbers
 $('body').on('keypress', '.num-only', function(e) {
