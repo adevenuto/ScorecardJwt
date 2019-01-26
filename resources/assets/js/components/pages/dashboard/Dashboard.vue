df6417085615f25b08577eea359a0356c13f0c42
adevenuto <anthonydevenuto@gmail.com>
2019-01-26 01:29:56 +0000

    building out course form ui

--- resources/assets/js/components/pages/dashboard/Dashboard.vue
+++ resources/assets/js/components/pages/dashboard/Dashboard.vue
@@ -6,7 +6,6 @@
 					<div class="my-courses-text">
 						My Courses
 					</div>
-					
 					<svg @click="courseFormToggle" class="plus-btn" width="39" height="39" viewBox="0 0 39 39" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M21.327 17.673h7.846a1.827 1.827 0 0 1 0 3.654h-7.846v7.846a1.827 1.827 0 0 1-3.654 0v-7.846H9.827a1.827 1.827 0 0 1 0-3.654h7.846V9.827a1.827 1.827 0 0 1 3.654 0v7.846z" fill="#43454a"/><circle stroke="#43454a" stroke-width="3" cx="19.5" cy="19.5" r="18"/></g></svg>
 				</div>
 				<div class="divider mb-2"></div>
@@ -14,26 +13,26 @@
 				<transition name="fade">
 				<div v-if="courseForm" class="slide-form">
 					<form action="">
-						<header v-if="step1">Create A New Course</header>
-						<header v-if="step2">Create Holes for {{formData.name}}</header>
+						<header v-if="step1">Create A New Golf Course</header>
+						<header v-if="step2">Create Holes for {{formData.golfClub.name}}</header>
 						<transition name="fade">
-							<div v-if="step1" class="step step-1">
+							<div v-show="step1" class="step step-1">
 								<div class="form-left"> 
 									<div class="form-group">
 										<label>Name:</label>
-										<input v-model="formData.name" type="text" class="form-control" required>
+										<input v-model="formData.golfClub.name" type="text" class="form-control" required>
 									</div>
 									<div class="form-group">
 										<label>Address:</label>
-										<input v-model="formData.address" type="text" class="form-control" required>
+										<input v-model="formData.golfClub.address" type="text" class="form-control" required>
 									</div>
 									<div class="form-group">
 										<label>Email:</label>
-										<input v-model="formData.email" type="email" class="form-control" required>
+										<input v-model="formData.golfClub.email" type="email" class="form-control" required>
 									</div>
 									<div class="form-group">
 										<label>Phone:</label>
-										<input v-model="formData.phone" type="text" class="form-control" required>
+										<input v-model="formData.golfClub.phone" type="text" class="form-control" required>
 									</div>
 								</div>
 								<div class="form-divider"></div>
@@ -41,7 +40,7 @@
 									<div class="slider-switch">
 										<label for="driving-range"> 
 											<div class="text">Driving Range</div>
-											<input v-model="formData.drivingRange" type="checkbox" id="driving-range">
+											<input v-model="formData.golfClub.drivingRange" type="checkbox" id="driving-range">
 											<span class="slider-switch-container">
 											<span class="slider-switch-toggle"></span>
 											</span>
@@ -50,7 +49,7 @@
 									<div class="slider-switch">
 										<label for="putting-green"> 
 											<div class="text">Putting Green</div>
-											<input v-model="formData.puttingGreen" type="checkbox" id="putting-green">
+											<input v-model="formData.golfClub.puttingGreen" type="checkbox" id="putting-green">
 											<span class="slider-switch-container">
 											<span class="slider-switch-toggle"></span>
 											</span>
@@ -59,7 +58,7 @@
 									<div class="slider-switch">
 										<label for="caddie"> 
 											<div class="text">Caddie</div>
-											<input v-model="formData.caddie" type="checkbox" id="caddie">
+											<input v-model="formData.golfClub.caddie" type="checkbox" id="caddie">
 											<span class="slider-switch-container">
 											<span class="slider-switch-toggle"></span>
 											</span>
@@ -68,7 +67,7 @@
 									<div class="slider-switch">
 										<label for="ProShop"> 
 											<div class="text">Pro Shop</div>
-											<input v-model="formData.proShop" type="checkbox" id="ProShop">
+											<input v-model="formData.golfClub.proShop" type="checkbox" id="ProShop">
 											<span class="slider-switch-container">
 											<span class="slider-switch-toggle"></span>
 											</span>
@@ -78,9 +77,46 @@
 							</div> <!-- step1 end -->
 						</transition>
 						<transition name="fade">
-						<div v-if="step2" class="step step2">
-							<h1>Hello</h1>
-						</div> <!-- step1 end -->
+
+
+
+						<div v-show="step2" class="step step-2">
+							<div class="step2-container">
+								<div class="hole-group">
+									<div class="header">
+										If this golf club is only 9 or 18 holes <strong>Do Not</strong>	fill in this field. However, if this golf club has multiple (individually named) courses on the property, add the name of the course you will be playing (additional courses can be added later).
+									</div>
+									<div class="form-group">
+										<label>Hole Group Name:</label>
+										<input v-model="formData.holeGroupName" type="text" class="form-control" required>
+									</div>
+								</div>
+								<div class="hole-count-teebox">
+									<div class="hole-count">
+										<div>Number of holes</div>
+										<label :class="[{'selected': formData.holeCount == '9'},'select-btn']" for="hole-count-9">9</label>
+										<input type="radio" class="hide" v-model="formData.holeCount" value="9" name="hole-count" id="hole-count-9">
+										<label :class="[{'selected': formData.holeCount == '18'},'select-btn']" for="hole-count-18">18</label>
+										<input type="radio" class="hide" v-model="formData.holeCount" value="18" name="hole-count" id="hole-count-18">
+									</div>
+									<div class="teebox">
+										<div>Teebox</div>
+										<select class="custom-select" v-model="formData.teeBox">
+											<option :value="null">Select a teebox</option>
+											<option value="tournament">Tournament</option>
+											<option value="back">Back</option>
+											<option value="middle">Middle</option>
+											<option value="front">Front</option>
+										</select>
+									</div>
+								</div>
+								<div class="divider"></div>
+							</div>
+						</div> <!-- step2 end -->
+
+
+
+
 						</transition>
 						<div class="form-nav">
 							<template v-if="step1">
@@ -127,21 +163,24 @@
 		data() {
 			return {
 				formData: {
-					name: null,
-					address: null,
-					email: null,
-					phone: null,
-					drivingRange: false,
-					puttingGreen: false,
-					caddie: false,
-					proShop: false,
-					holes: [
-
-					]
+					golfClub: {
+						name: null,
+						address: null,
+						email: null,
+						phone: null,
+						drivingRange: false,
+						puttingGreen: false,
+						caddie: false,
+						proShop: false,
+					},
+					teeBox: null,
+					holeGroupName: null,
+					holeCount: null,
+					holes: []
 				},
-				courseForm: false, 
-				step1: true,
-				step2: false
+				courseForm: true, 
+				step1: false,
+				step2: true
 			}
 		},
 		computed: {
@@ -232,7 +271,7 @@
 			font-size: .9rem;
 			font-weight: bold;
 		}
-		input {
+		input, select {
 			border: 1px solid #000 !important;
 		}
 		.form-group {
@@ -258,7 +297,34 @@
 			}
 		}
 		.step.step-2 {
-			
+			.step2-container {
+				.hole-group {
+					border: 1px solid #000;
+					background: #f3f3f3;
+					padding: 10px;
+					.header {
+						font-size: .71rem;
+    					padding-bottom: 5px;
+					}
+				}
+			}
+			.hole-count-teebox {
+				display: flex;
+				justify-content: space-between;
+				margin: 10px;
+				.hole-count .select-btn {
+					padding: calc(.3rem + 2px) 35px;
+					border: 1px solid #000;
+					margin-bottom: 10px;
+					&:hover {
+						background: #f9f9f9;
+					}
+				}
+				.hole-count .select-btn.selected {
+					background: #0e6f0e;
+    				color: #fff;
+				}
+			}
 		}
 		.form-nav {
 			display: flex;
@@ -268,10 +334,31 @@
 			background: #fff;
 			.nav-btn {
 				border-radius: $g-btn-radius;
-				padding: 10px 30px;
+				padding: 10px 22px;
+				cursor: pointer;
 			}
 		}
 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	.slider-switch {
 		margin-bottom: 15px;
 		input {
@@ -292,15 +379,15 @@
 			background: #D8D8D8;
 			box-shadow: inset 0px 0px 4px 0px #999;
 			.slider-switch-toggle {
-			position: absolute;
-			left: -1px;
-			top: -1px;
-			height: 20px;
-			width: 20px;
-			border-radius: 50%;
-			background: #a2a2a2;
-    		border: 2px solid #a2a2a2;
-			transition: 250ms;
+				position: absolute;
+				left: -1px;
+				top: -1px;
+				height: 20px;
+				width: 20px;
+				border-radius: 50%;
+				background: #fff;
+				border: 2px solid #a2a2a2;
+				transition: 250ms;
 			}
 		}
 		input:checked + .slider-switch-container .slider-switch-toggle {
@@ -358,6 +445,16 @@
 				margin: 15px 0;
 				height: 2px;
 				width: 100%;
+			}
+			.step.step-2 .hole-count-teebox .hole-count .select-btn {
+				padding: calc(.3rem + 2px) 27px;
+			}
+		}
+	}
+	@media (max-width: 374px) {
+		.slide-form { 
+			.step.step-2 .hole-count-teebox {
+				flex-direction: column;
 			}
 		}
 	}
